1a134f8417f948e60e8a67e1ed79e47097e122c5
Abdiqani <abdiqaniadan254@gmail.com>
2024-01-09 00:50:10 +0300

    Created initial tables schema

---
+++ app/migrations/versions/abd45595501b_create_inital_tables.py
@@ -0,0 +1,24 @@
+"""Create inital tables
+
+Revision ID: abd45595501b
+Revises: 
+Create Date: 2024-01-09 00:43:47.875835
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'abd45595501b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- app/models.py
+++ app/models.py
@@ -11,3 +11,38 @@ Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session() 
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    price = Column(Integer())
+
+    def __repr__(self):
+        return f"Restaurant(id={self.id}, name={self.name}, price={self.price})"
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+
+
+    def __repr__(self):
+        return f'Customer(id={self.id}, first_name={self.first_name}, last_name={self.last_name})'
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    star_rating = Column(Integer())
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+
+    def __repr__(self):
+        return f'Review(id={self.id}, comments={self.comments}, star_rating={self.star_rating})'
